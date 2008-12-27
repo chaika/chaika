eef34d5812487c8947fc4a69ace0c6fe44942304
flyson <flyson.moz@gmail.com>
2008-12-27 14:08:28 +0000

    スレッド表示のリファラ制御
    git-svn-id: http://chaika.googlecode.com/svn/trunk@52 86a2c536-bba4-11dd-9405-3d0920048215

--- chaika/defaults/preferences/chaika-pref.js
+++ chaika/defaults/preferences/chaika-pref.js
@@ -10,6 +10,8 @@ pref("extensions.chaika.server_port.firefox", 8823);
 pref("extensions.chaika.server_port.seamonkey", 8824);
 pref("extensions.chaika.server_port.other", 8825);
 
+pref("extensions.chaika.refController.enabled", true);
+
 pref("extensions.chaika.logger_level", 0);
 
 pref("extensions.chaika.history_expire_days", 60);
