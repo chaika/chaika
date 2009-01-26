6c9306fc9a359cb53801c152b07695ad184a3220
flyson <flyson.moz@gmail.com>
2009-01-26 10:40:25 +0000

    ブラウザオーバーレイ関連ファイルの整理
    git-svn-id: http://chaika.googlecode.com/svn/trunk@146 86a2c536-bba4-11dd-9405-3d0920048215

--- chaika/chrome/content/chaika/browser/overlay.js
+++ chaika/chrome/content/chaika/browser/overlay.js
@@ -1,40 +1,3 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is bbs2chreader.
- *
- * The Initial Developer of the Original Code is
- * flyson.
- * Portions created by the Initial Developer are Copyright (C) 2004
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *    flyson <flyson at users.sourceforge.jp>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
 Components.utils.import("resource://chaika-modules/ChaikaCore.js");
 
 var ChaikaBrowserOverlay = {};
@@ -63,7 +26,7 @@ ChaikaBrowserOverlay.contextMenu = {
 
 	showMenu: function contextMenu_showMenu(aEvent){
 		if(aEvent.originalTarget.id != "contentAreaContextMenu") return;
-		document.getElementById("context-bbs2chreader").hidden = true;
+		document.getElementById("context-chaika").hidden = true;
 
 		if(!gContextMenu || !gContextMenu.onLink) return;
 
@@ -71,14 +34,14 @@ ChaikaBrowserOverlay.contextMenu = {
 		if(pathname.indexOf("/test/read.cgi/")==-1 &&
 				pathname.indexOf("/bbs/read.cgi/")==-1) return;
 
-		document.getElementById("context-bbs2chreader").hidden = false;
+		document.getElementById("context-chaika").hidden = false;
 	},
 
 
 	openThread: function contextMenu_openThread(aAddTab){
 		if(!gContextMenu || !gContextMenu.onLink) return;
 
-				// „Çπ„É¨„ÉÉ„ÉâË°®Á§∫Êï∞„ÅÆÂà∂Èôê
+				// ÉXÉåÉbÉhï\é¶êîÇÃêßå¿
 		var disregardURLOption = ChaikaCore.pref.getBool(
 				"browser_contextmenu_disregard_url_option");
 
@@ -114,7 +77,7 @@ ChaikaBrowserOverlay.statusbar = {
 
 	webProgress: {
 		onLocationChange: function(aWebProgress, aRequest, aLocation){
-			var b2rstatusbar = document.getElementById("statusbar-bbs2chreader");
+			var b2rstatusbar = document.getElementById("statusbar-chaika");
 			b2rstatusbar.showCheck();
 		},
 		onStateChange: function(){},
