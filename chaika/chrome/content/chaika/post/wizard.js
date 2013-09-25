2812c49f2b0fe6bfeb5a9a2f7dd06cb3521e3c25
nodaguti <nodaguti@gmail.com>
2013-09-25 17:03:39 +0900

    nsIFormHistory2 を FormHistory.jsm で書き換え closes #39

--- chaika/chrome/content/chaika/post/wizard.js
+++ chaika/chrome/content/chaika/post/wizard.js
@@ -446,13 +446,40 @@ var FormPage = {
 
 
 	addFormHistory: function FormPage_addFormHistory(){
-		var formHistory	= Cc["@mozilla.org/satchel/form-history;1"]
-				.getService(Ci.nsIFormHistory2);
-		if(this._nameForm && this._nameForm.value){
-			formHistory.addEntry("chaika-post-name-history", this._nameForm.value);
-		}
-		if(this._nameForm && this._mailForm.value){
-			formHistory.addEntry("chaika-post-mail-history", this._mailForm.value);
+		try{
+			//Firefox 24+
+			let FormHistory = (Components.utils.import("resource://gre/modules/FormHistory.jsm", {})).FormHistory;
+
+			var changes = [];
+
+			if(this._nameForm && this._nameForm.value){
+				changes.push({
+					op: "add",
+					fieldname: 'chaika-post-name-history',
+					value: this._nameForm.value
+				});
+			}
+			if(this._nameForm && this._mailForm.value){
+				changes.push({
+					op: "add",
+					fieldname: 'chaika-post-mail-history',
+					value: this._mailForm.value
+				});
+			}
+
+			if(changes.length > 0){
+				FormHistory.update(changes);
+			}
+		}catch(ex){
+			//Firefox 17-23
+			var formHistory	= Cc["@mozilla.org/satchel/form-history;1"].getService(Ci.nsIFormHistory2);
+
+			if(this._nameForm && this._nameForm.value){
+				formHistory.addEntry("chaika-post-name-history", this._nameForm.value);
+			}
+			if(this._nameForm && this._mailForm.value){
+				formHistory.addEntry("chaika-post-mail-history", this._mailForm.value);
+			}
 		}
 	},
 
