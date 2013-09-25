9f7f08e416b87ec91adb78a965767a20e5e0378a
nodaguti <nodaguti@gmail.com>
2013-09-25 16:13:43 +0900

    -moz-initial と initial を併記するようにした closes #42

--- chaika/chrome/content/chaika/defaults/skin/script.js
+++ chaika/chrome/content/chaika/defaults/skin/script.js
@@ -83,7 +83,8 @@ var $ = {
 	 * @return {Node}
 	 */
 	show: function(element){
-		$.css(element, { display: '-moz-initial' });
+		$.css(element, { display: '-moz-initial' });  // For Firefox 17-23
+		$.css(element, { display: 'initial' });
 		return element;
 	},
 
