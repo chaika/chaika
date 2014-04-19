26a8e9d836fc2518278b4dbe586a97b35727a2c4
nodaguti <nodaguti@gmail.com>
2014-04-19 14:54:47 +0900

    Firefox 24 で ff2ch.syoboi.jp プラグインが動かないのを修正

--- chaika/chrome/content/chaika/defaults/search/ff2ch.search.js
+++ chaika/chrome/content/chaika/defaults/search/ff2ch.search.js
@@ -8,6 +8,7 @@ try{
     Components.utils.import("resource://gre/modules/commonjs/sdk/core/promise.js");
 }
 
+
 var Ff2ch = {
 
     id: 'ff2ch.syoboi.jp',
@@ -80,4 +81,37 @@ var Ff2ch = {
         this._defer.resolve(boards);
     },
 
+}
+
+
+//Polyfill for Firefox 24
+//Copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function(predicate) {
+            if (this == null) {
+                throw new TypeError('Array.prototype.find called on null or undefined');
+            }
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this);
+            var length = list.length >>> 0;
+            var thisArg = arguments[1];
+            var value;
+
+            for (var i = 0; i < length; i++) {
+                if (i in list) {
+                    value = list[i];
+                    if (predicate.call(thisArg, value, i, list)) {
+                        return value;
+                    }
+                }
+            }
+            return undefined;
+        }
+    });
 }
