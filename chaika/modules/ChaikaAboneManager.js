c39a56d2eaf50f7bf3dfd3ad6ce27c31b8898b97
nodaguti <nodaguti@gmail.com>
2014-04-27 22:30:10 +0900

    ChaikaAboneManager.js をリファクタリングして NGEx を追加

--- chaika/modules/ChaikaAboneManager.js
+++ chaika/modules/ChaikaAboneManager.js
@@ -38,20 +38,10 @@
 
 EXPORTED_SYMBOLS = ["ChaikaAboneManager"];
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import('resource://gre/modules/Services.jsm');
 Components.utils.import("resource://chaika-modules/ChaikaCore.js");
 
-
-const Ci = Components.interfaces;
-const Cc = Components.classes;
-const Cr = Components.results;
-
-
-/** @ignore */
-function makeException(aResult){
-    var stack = Components.stack.caller.caller;
-    return new Components.Exception("exception", aResult, stack);
-}
-
+const { interfaces: Ci, classes: Cc, results: Cr, utils: Cu } = Components;
 
 /** @ignore */
 var UniConverter = {
@@ -74,17 +64,25 @@ var UniConverter = {
 
 var ChaikaAboneManager = {
 
+    /** あぼーんの種類を表す定数 */
+
     ABONE_TYPE_NAME : 0,
     ABONE_TYPE_MAIL : 1,
     ABONE_TYPE_ID   : 2,
     ABONE_TYPE_WORD : 3,
+    ABONE_TYPE_EX   : 4,
+
 
     /**
      * ブラウザ起動時のプロファイル読み込み後に一度だけ実行され、初期化処理を行う。
      * @private
      */
     _startup: function ChaikaAboneManager__startup(){
-        this._loadAboneData();
+        this.name = new AboneData(this.ABONE_TYPE_NAME, 'NGnames.txt');
+        this.mail = new AboneData(this.ABONE_TYPE_MAIL, 'NGaddrs.txt');
+        this.id = new AboneData(this.ABONE_TYPE_ID, 'NGid.txt');
+        this.word = new AboneData(this.ABONE_TYPE_WORD, 'NGwords.txt');
+        this.ex = new NGExAboneData(this.ABONE_TYPE_EX, 'NGEx.txt');
     },
 
 
@@ -93,163 +91,272 @@ var ChaikaAboneManager = {
      * @private
      */
     _quit: function ChaikaAboneManager__quit(){
-        this._saveAboneData();
+        this.name.uninit();
+        this.mail.uninit();
+        this.id.uninit();
+        this.word.uninit();
+        this.ex.uninit();
     },
 
 
-    _loadAboneData: function ChaikaAboneManager__loadAboneData(){
-        this._aboneData = new Array();
-        this._aboneData["name"] = this._loadNgFile("NGnames.txt");
-        this._aboneData["mail"] = this._loadNgFile("NGaddrs.txt");
-        this._aboneData["id"]   = this._loadNgFile("NGid.txt");
-        this._aboneData["word"] = this._loadNgFile("NGwords.txt");
+    /**
+     * あぼーんするべきかどうかを調べる
+     * @param {Object} aResData レスのデータ
+     * @param {String} aResData.name 名前
+     * @param {String} aResData.mail メール
+     * @param {String} aResData.id ID
+     * @param {String} aResData.msg 本文
+     * @param {String} aResData.date 書き込み日時
+     * @param {String} aResData.ip IPアドレス
+     * @param {String} aResData.host ホスト
+     * @param {String} aResData.be BeID
+     * @param {Number} aResData.baseBe Be基礎番号
+     * @param {ChaikaThread} aResData.thread スレッド
+     * @return {Boolean}
+     */
+    shouldAbone: function ChaikaAboneManager_shouldAbone(aResData){
+        return this.name.shouldAbone(aResData.name) ||
+               this.mail.shouldAbone(aResData.mail) ||
+               this.id.shouldAbone(aResData.id) ||
+               this.word.shouldAbone(aResData.msg) ||
+               this.ex.shouldAbone(aResData);
     },
-
-
-    _loadNgFile: function ChaikaAboneManager__loadNgFile(aNgFileName){
-        var ngFile = ChaikaCore.getDataDir();
-        ngFile.appendRelativePath(aNgFileName);
-        if(!ngFile.exists()) return new Array();
-
-        var contentLine = ChaikaCore.io.readData(ngFile).split("\n");
-        var resultArray = new Array();
-            // 空白行は読み込まない
-        for(let [i, line] in Iterator(contentLine)){
-            if(line) resultArray.push(line);
-        }
-        return resultArray;
-    },
-
-
-    _saveAboneData: function ChaikaAboneManager__saveAboneData(){
-        this._saveNgFile("NGnames.txt", this._aboneData["name"]);
-        this._saveNgFile("NGaddrs.txt", this._aboneData["mail"]);
-        this._saveNgFile("NGid.txt",    this._aboneData["id"]);
-        this._saveNgFile("NGwords.txt", this._aboneData["word"]);
-    },
-
-
-    _saveNgFile: function ChaikaAboneManager__saveNgFile(aNgFileName, aboneDataArray){
-        var ngFile = ChaikaCore.getDataDir();
-        ngFile.appendRelativePath(aNgFileName);
-        ChaikaCore.io.writeData(ngFile, aboneDataArray.join("\n"), false);
-    },
-
-
-    shouldAbone: function ChaikaAboneManager_shouldAbone(aName, aMail, aID, aMsg){
-        function checkFunc(aElement, aIndex, aArray){
-            return this.indexOf(aElement) != -1;
-        }
-        if(this._aboneData["name"].some(checkFunc, aName)) return true;
-        if(this._aboneData["mail"].some(checkFunc, aMail)) return true;
-        if(this._aboneData["id"].some(checkFunc, aID)) return true;
-        if(this._aboneData["word"].some(checkFunc, aMsg)) return true;
-
-        return false;
-    },
-
-
-    getAboneData: function ChaikaAboneManager_getAboneData(aType){
-        var ngArray;
-        switch(aType){
-            case this.ABONE_TYPE_NAME:
-                ngArray = this._aboneData["name"];
-                break;
-            case this.ABONE_TYPE_MAIL:
-                ngArray = this._aboneData["mail"];
-                break;
-            case this.ABONE_TYPE_ID:
-                ngArray = this._aboneData["id"];
-                break;
-            case this.ABONE_TYPE_WORD:
-                ngArray = this._aboneData["word"];
-                break;
-            default:
-                return null;
-        }
-
-        var unicodeConverter = Cc["@mozilla.org/intl/scriptableunicodeconverter"]
-                    .createInstance(Ci.nsIScriptableUnicodeConverter);
-        unicodeConverter.charset = "Shift_JIS";
-
-        var resultArray = ngArray.map(function testFunc(aElement, aIndex, aArray){
-                return unicodeConverter.ConvertToUnicode(aElement);
-        });
-        return resultArray;
-    },
-
-
-    addAbone: function ChaikaAboneManager_addAbone(aWord, aType){
-        var unicodeConverter = Cc["@mozilla.org/intl/scriptableunicodeconverter"]
-                    .createInstance(Ci.nsIScriptableUnicodeConverter);
-        unicodeConverter.charset = "Shift_JIS";
-
-        var sjisWord = unicodeConverter.ConvertFromUnicode(aWord);
-
-        var ngArray;
-        switch(aType){
-            case this.ABONE_TYPE_NAME:
-                ngArray = this._aboneData["name"];
-                break;
-            case this.ABONE_TYPE_MAIL:
-                ngArray = this._aboneData["mail"];
-                break;
-            case this.ABONE_TYPE_ID:
-                ngArray = this._aboneData["id"];
-                break;
-            case this.ABONE_TYPE_WORD:
-                ngArray = this._aboneData["word"];
-                break;
-            default:
-                return;
-        }
-
-            // 二重登録の禁止
-        if(ngArray.indexOf(sjisWord) != -1) return;
-
-        ngArray.push(sjisWord);
-
-        var os = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
-        var type = Cc["@mozilla.org/supports-PRInt32;1"].createInstance(Ci.nsISupportsPRInt32);
-        type.data = aType;
-
-        os.notifyObservers(type, "b2r-abone-data-add", aWord);
-    },
-
-
-    removeAbone: function ChaikaAboneManager_removeAbone(aWord, aType){
-        var unicodeConverter = Cc["@mozilla.org/intl/scriptableunicodeconverter"]
-                    .createInstance(Ci.nsIScriptableUnicodeConverter);
-        unicodeConverter.charset = "Shift_JIS";
-
-        var sjisWord = unicodeConverter.ConvertFromUnicode(aWord);
-
-        var ngArray;
-        switch(aType){
-            case this.ABONE_TYPE_NAME:
-                ngArray = this._aboneData["name"];
-                break;
-            case this.ABONE_TYPE_MAIL:
-                ngArray = this._aboneData["mail"];
-                break;
-            case this.ABONE_TYPE_ID:
-                ngArray = this._aboneData["id"];
-                break;
-            case this.ABONE_TYPE_WORD:
-                ngArray = this._aboneData["word"];
-                break;
-            default:
-                return;
-        }
-
-        var wordIndex = ngArray.indexOf(sjisWord);
-        if(wordIndex != -1) ngArray.splice(wordIndex, 1);
-
-        var os = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
-        var type = Cc["@mozilla.org/supports-PRInt32;1"].createInstance(Ci.nsISupportsPRInt32);
-        type.data = aType;
-        os.notifyObservers(type, "b2r-abone-data-remove", aWord);
-
-    }
 
 };
+
+
+
+/**
+ * あぼーんデータ
+ */
+function AboneData(aNgType, aFileName){
+    this._init(aNgType, aFileName);
+}
+
+AboneData.prototype = {
+
+    _init: function(aNgType, aFileName){
+        this._ngType = aNgType;
+
+        this._ngFile = ChaikaCore.getDataDir();
+        this._ngFile.appendRelativePath(aFileName);
+
+        this._loadNgData();
+    },
+
+
+    uninit: function(){
+        this._saveNgData();
+    },
+
+
+    _loadNgData: function(){
+        if(!this._ngFile.exists())
+            return this._data = [];
+
+        //Shift-JISのまま読み込みたいので、readStringではなくreadDataを用いる
+        //空白行を除いて読み込む
+        this._data = ChaikaCore.io.readData(this._ngFile)
+                                  .split('\n')
+                                  .filter((line) => !!line);
+    },
+
+
+    _saveNgData: function(){
+        ChaikaCore.io.writeData(this._ngFile, this._data.join("\n"), false);
+    },
+
+
+    shouldAbone: function(aResData){
+        return this._data.some((ngData) => aResData.contains(ngData));
+    },
+
+
+    getNgData: function(){
+        return this._data.map((item) => UniConverter.fromSJIS(item));
+    },
+
+
+    add: function(aWord){
+        let sjisWord = UniConverter.toSJIS(aWord);
+
+        //2重登録を防ぐ
+        if(this._data.indexOf(sjisWord) !== -1){
+            return;
+        }
+
+        this._data.push(sjisWord);
+
+        //通知する
+        let type = Cc["@mozilla.org/supports-PRInt32;1"].createInstance(Ci.nsISupportsPRInt32);
+        type.data = this._ngType;
+
+        Services.obs.notifyObservers(type, "b2r-abone-data-add", aWord);
+    },
+
+    remove: function(aWord){
+        let sjisWord = UniConverter.toSJIS(aWord);
+        let index = this._data.indexOf(sjisWord);
+
+        if(index !== -1){
+            this._data.splice(index, 1);
+
+            //通知する
+            let type = Cc["@mozilla.org/supports-PRInt32;1"].createInstance(Ci.nsISupportsPRInt32);
+            type.data = this._ngType;
+
+            Services.obs.notifyObservers(type, "b2r-abone-data-remove", aWord);
+        }
+    },
+
+};
+
+
+// NGEx で正規表現のデータがJSONから抜け落ちるのを防ぐ
+RegExp.prototype.toJSON = RegExp.prototype.toString;
+
+
+/**
+ * あぼーんデータ (NGEx)
+ */
+function NGExAboneData(aNgType, aFileName){
+    AboneData.apply(this, arguments);
+}
+
+NGExAboneData.prototype = Object.create(AboneData.prototype, {
+
+    _loadNgData: {
+        value: function(){
+            if(!this._ngFile.exists())
+                return this._data = [];
+
+            //Shift-JISのまま読み込みたいので、readStringではなくreadDataを用いる
+            //空白行を除いて読み込む
+            this._data = ChaikaCore.io.readData(this._ngFile)
+                                      .split('\n')
+                                      .filter((line) => !!line);
+
+            this._dataObj = this._data.map((item) => JSON.parse(item));
+        }
+    },
+
+
+    shouldAbone: {
+        value: function(aResData){
+            return this._dataObj.some((ngData) => {
+                if(ngData.match === 'all')
+                    return ngData.rules.every((rule) => this._matchRule(rule, aResData));
+
+                else if(ngData.match === 'any')
+                    return ngData.rules.some((rule) => this._matchRule(rule, aResData));
+            });
+        }
+    },
+
+
+    _matchRule: {
+        value: function(aRule, aResData){
+            let target = aResData[aRule.target];
+
+            if(aRule.regexp){
+                return aRule.query.test(target);
+            }else{
+                if(aRule.ignoreCase){
+                    target = target.toLowerCase();
+                }
+
+                switch(aRule.condition){
+                    case 'contains':
+                        return target.contains(aRule.query);
+
+                    case 'notContain':
+                        return !target.contains(aRule.query);
+
+                    case 'equals':
+                        return target === aRule.query;
+
+                    case 'notEqual':
+                        return target !== aRule.query;
+
+                    case 'startsWith':
+                        return target.startsWith(aRule.query);
+
+                    case 'endsWith':
+                        return target.endsWith(aRule.query);
+
+                    default:
+                        return false;
+                }
+            }
+        }
+    },
+
+
+    add: {
+        value: function(aNGData){
+            //データの補正
+            aNGData.rules.forEach((rule) => {
+                if(rule.regexp){
+                    rule.query = new RegExp(rule.query, rule.ignoreCase ? 'i' : '');
+                }else{
+                    if(rule.ignoreCase){
+                        rule.query = rule.ruery.toLowerCase();
+                    }
+                }
+            });
+
+            let jsonData = JSON.stringify(aNGData);
+            let sjisWord = UniConverter.toSJIS(jsonData);
+            let sjisJsonData = JSON.parse(sjisWord);
+
+            //2重登録を防ぐ
+            if(this._data.indexOf(sjisWord) !== -1){
+                return;
+            }
+
+            this._data.push(sjisWord);
+            this._dataObj.push(sjisJsonData);
+
+            //通知する
+            let type = Cc["@mozilla.org/supports-PRInt32;1"].createInstance(Ci.nsISupportsPRInt32);
+            type.data = this._ngType;
+
+            Services.obs.notifyObservers(type, "b2r-abone-data-add", jsonData);
+        }
+    },
+
+    remove: {
+        value: function(aNGData){
+            //データの補正
+            aNGData.rules.forEach((rule) => {
+                if(rule.regexp){
+                    rule.query = new RegExp(rule.query, rule.ignoreCase ? 'i' : '');
+                }else{
+                    if(rule.ignoreCase){
+                        rule.query = rule.ruery.toLowerCase();
+                    }
+                }
+            });
+
+            let jsonData = JSON.stringify(aNGData);
+            let sjisWord = UniConverter.toSJIS(jsonData);
+            let index = this._data.indexOf(sjisWord);
+
+            if(index !== -1){
+                this._data.splice(index, 1);
+                this._dataObj.splice(index, 1);
+
+                //通知する
+                let type = Cc["@mozilla.org/supports-PRInt32;1"].createInstance(Ci.nsISupportsPRInt32);
+                type.data = this._ngType;
+
+                Services.obs.notifyObservers(type, "b2r-abone-data-remove", jsonData);
+            }
+        }
+    },
+
+});
+
+NGExAboneData.constructor = NGExAboneData;
+
+
