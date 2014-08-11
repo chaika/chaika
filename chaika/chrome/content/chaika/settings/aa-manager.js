c779513c96b4f19f3a78f150680538006ccd1f1f
nodaguti <nodaguti@gmail.com>
2014-08-11 17:24:21 +0900

    [WIP] メニュー項目の実装

--- chaika/chrome/content/chaika/settings/aa-manager.js
+++ chaika/chrome/content/chaika/settings/aa-manager.js
@@ -1,6 +1,46 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is chaika.
+ *
+ * The Initial Developer of the Original Code is
+ * chaika.xrea.jp
+ * Portions created by the Initial Developer are Copyright (C) 2014
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *    flyson <flyson.moz at gmail.com>
+ *    nodaguti <nodaguti at gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 
 Components.utils.import('resource://chaika-modules/ChaikaCore.js');
 Components.utils.import('resource://chaika-modules/ChaikaAA.js');
+
+const { interfaces: Ci, classes: Cc, results: Cr, utils: Cu } = Components;
 
 
 var gAAManager = {
@@ -53,7 +93,74 @@ var gAAManager = {
             this._aaLabel.value = aa.getAttribute('title');
             this._aaTextbox.value = aa.textContent;
         }
-    }
+    },
+
+
+    insertFolder: function(title){
+        this._view.appendItem('folder', title || '新規フォルダ');
+    },
+
+
+    insertAA: function(title, value){
+        this._view.appendItem('aa', title || '新規 AA', value);
+    },
+
+
+    loadAAList: function(){
+        let fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
+
+        fp.init(window, 'AAList.txt の選択', Ci.nsIFilePicker.modeOpen);
+        fp.appendFilters(Ci.nsIFilePicker.filterAll | Ci.nsIFilePicker.filterText);
+
+        let rv = fp.show();
+
+        if(rv === Ci.nsIFilePicker.returnOK || rv === Ci.nsIFilePicker.returnReplace){
+            let file = fp.file;
+            let aaListString = ChaikaCore.io.readString(file, 'Shift_JIS');
+            let aaGroupTitles = aaListString.match(/^\[[^\]]+\]$/mg);
+            let aaGroups = aaListString.split(/^\[[^\]]+\]$/m);
+            let aaTables = {};
+
+            aaGroupTitles = Array.slice(aaGroupTitles).map((title) => {
+                return title.replace(/^\[/, '').replace(/\]$/, '');
+            });
+
+            aaGroups.shift();
+
+            aaGroups.forEach((group, index) => {
+                if(aaGroupTitles[index].toLowerCase() === 'aalist'){
+                    //一行AA
+                    group.split(/[\r\n]+/).forEach((line) => {
+                        //複数行AAのタイトル
+                        if(line.startsWith('*')) return;
+
+                        aaTables[line] = line;
+                    });
+                }else{
+                    //複数行AA
+                    aaTables[aaGroupTitles[index]] = group;
+                }
+            });
+
+            this.insertFolder('AAList.txt');
+
+            for(let title in aaTables){
+                if(title && aaTables.hasOwnProperty(title) && aaTables[title]){
+                    this.insertAA(title, aaTables[title]);
+                }
+            }
+        }
+    },
+
+
+    saveAA: function(){
+        this._view.changeItemAt(this._view.selectedIndex, this._aaLabel.value, this._aaTextbox.value);
+    },
+
+
+    removeItem: function(){
+        this._view.removeItemAt(this._view.selectedIndex);
+    },
 
 
 };
@@ -79,16 +186,122 @@ AATreeView.prototype = {
     _buildVisibleNodes: function(){
         this._visibleNodes = Array.slice(this._xml.querySelectorAll(':root > folder, folder[opened] > *'));
 
-        ChaikaCore.logger.debug(this._visibleNodes.length);
+        this._visibleNodes = this._visibleNodes.filter((node) => {
+            if(node.matches){
+                //Firefox 34+
+                return !node.matches('folder:not([opened]) *');
+            }else{
+                //Firefox 34-
+                return !node.mozMatchesSelector('folder:not([opened]) *');
+            }
+        });
 
         this._visibleNodes.forEach((node) => {
             node._parentIndex = this._visibleNodes.indexOf(node.parentNode);
         });
     },
 
+    appendItem: function(type, title, value){
+        ChaikaCore.logger.debug('Append Item:', type, title, value);
+
+        let selectedIndex = this.selectedIndex;
+        let parentIndex;
+
+
+        if(selectedIndex === -1){
+            selectedIndex = this.rowCount - 1;
+        }
+
+
+        if(this.isContainer(selectedIndex) && type === 'aa'){
+            parentIndex = selectedIndex;
+        }else{
+            parentIndex = this.getParentIndex(selectedIndex);
+        }
+
+        title = this._getUniqueName(parentIndex, title);
+
+
+        let parentNode = this._visibleNodes[parentIndex] || this._xml;
+        let node = document.createElement(type);
+
+        node.setAttribute('title', title);
+        node.setAttribute('level', parentIndex > -1 ? this.getLevel(parentIndex) + 1 : 0);
+
+        if(value){
+            node.appendChild(document.createTextNode(value));
+        }
+
+        parentNode.appendChild(node);
+
+
+        //open parent folders
+        let folder = node;
+
+        while(folder = folder.parentNode){
+            if(folder.nodeName === 'folder'){
+                folder.setAttribute('opened', 'true');
+            }
+        }
+
+        this._buildVisibleNodes();
+
+
+        let newIndex = this._visibleNodes.indexOf(parentNode.lastChild);
+
+        this._treeBoxObject.rowCountChanged(newIndex, 1);
+        this.selection.select(newIndex);
+        this._treeBoxObject.ensureRowIsVisible(newIndex);
+        this._treeBoxObject.treeBody.focus();
+    },
+
+    _getUniqueName: function(parentIndex, name){
+        let parentNode = this._visibleNodes[parentIndex] || this._xml;
+        let appendix = 2;
+
+        if(!parentNode.querySelector('[title="' + name + '"]')){
+            return name;
+        }
+
+        while(parentNode.querySelector('[title="' + name + ' ' + appendix + '"]')){
+            appendix++;
+        }
+
+        return name + ' ' + appendix;
+    },
+
+    changeItemAt: function(index, title, value){
+        let node = this._visibleNodes[index];
+
+        node.setAttribute('title', title);
+        node.removeChild(node.firstChild);
+        node.appendChild(document.createTextNode(value));
+
+        this._treeBoxObject.invalidateRow(index);
+    },
+
+    removeItemAt: function(index){
+        this._visibleNodes[index].parentNode.removeChild(this._visibleNodes[index]);
+        this._buildVisibleNodes();
+        this._treeBoxObject.rowCountChanged(index, -1);
+        this.selection.clearSelection();
+    },
 
     get rowCount(){
         return this._visibleNodes.length;
+    },
+
+    get selectedIndex(){
+        let start = {};
+        let end = {};
+
+        this.selection.getRangeAt(0, start, end);
+
+        if(start.value === end.value){
+            return start.value;
+        }else{
+            return -1;
+        }
     },
 
     selection: null,
