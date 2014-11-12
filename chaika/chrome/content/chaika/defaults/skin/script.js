chaika/chaika

commit 13f2b97fd030d2202694593256d2f91306b66616

逆参照カウントの処理をより正確に行うようにした
アンカーポップアップと同様の処理を行うように改善

chaika/chrome/content/chaika/defaults/skin/script.js
@@ -478,9 +478,13 @@ var ResInfo = {
 
             //逆参照
             if(enableRefCount){
-                let anchors = $.klass('resBody', resNode).textContent.match(/>>?\d{1,4}(?:-\d{1,4})?/g);
+                let anchors = $.klass('resPointer', resNode, true);
 
-                if(anchors){
+                if(anchors && anchors.length > 0){
+                    anchors = anchors.reduce((ary, node) => {
+                        return ary.concat(node.textContent.match(/(?:\d{1,4}-\d{1,4}|\d{1,4}(?!-))/g));
+                    }, []);
+
                     anchors.forEach((anchor) => {
                         let [begin, end] = anchor.split('-');
 
@@ -1056,8 +1060,6 @@ Popup.Res = {
 
             return this._createContent(begin, end);
         })).then((popupContents) => {
-            console.log(popupContents);
-
             let fragment = document.createDocumentFragment();
             let shouldInvert = Prefs.get('pref-invert-res-popup-dir');
 

commit 1d1e16e867a6e650a0cd0e4508d0cf40582d7510

逆参照の処理が誤っていたのを修正
Reported by: http://anago.2ch.net/test/read.cgi/software/1415199627/213
Regressed from: 13f2b97

chaika/chrome/content/chaika/defaults/skin/script.js
@@ -488,7 +488,7 @@ var ResInfo = {
                     anchors.forEach((anchor) => {
                         let [begin, end] = anchor.split('-');
 
-                        begin = begin.substring(2) - 0;
+                        begin = begin - 0;
                         end = end ? end - 0 : begin;
 
                         if(begin < 1) begin = 1;
@@ -1058,7 +1058,7 @@ Popup.Res = {
             let [begin, end] = anchor.split('-');
             if(!end) end = begin;
 
-            return this._createContent(begin, end);
+            return this._createContent(begin-0, end-0);
         })).then((popupContents) => {
             let fragment = document.createDocumentFragment();
             let shouldInvert = Prefs.get('pref-invert-res-popup-dir');