ee44bd50ace438a6e5dc4c7ab60179bf310e478e
nodaguti <nodaguti@gmail.com>
2014-12-31 21:36:00 +0900

    Cosmetic change

--- chaika/modules/server/skin.js
+++ chaika/modules/server/skin.js
@@ -16,52 +16,56 @@ function SkinServerScript(){
 SkinServerScript.prototype = {
 
     start: function(aServerHandler){
-        var filePath = aServerHandler.request.url.filePath.substring(6);
+        var path = aServerHandler.request.url.filePath.substring(6);
+        var skinFile = this.resolveSkinFile(path);
 
-        if(!filePath){
+
+        // Send 404 if the file doesn't exist
+        if(!skinFile || !skinFile.exists()){
             aServerHandler.sendErrorPage(404, aServerHandler.request.url.spec);
             return;
         }
 
-        var skinFile = this.resolveSkinFile(filePath);
 
-            // File Not Found
-        if(!skinFile.exists()){
-            aServerHandler.sendErrorPage(404, aServerHandler.request.url.spec);
-            return;
-        }
-
-        var lastModifiedString = new Date(skinFile.lastModifiedTime).toUTCString();
-        aServerHandler.response.setHeader("Last-Modified", lastModifiedString);
+        // Set cache
+        var lastModified = new Date(skinFile.lastModifiedTime).toUTCString();
+        aServerHandler.response.setHeader("Last-Modified", lastModified);
         aServerHandler.response.setHeader("Cache-Control", "max-age=0, must-revalidate");
 
-            // If-Modified-Since が存在しファイルが更新されていなければ 304
-        if(aServerHandler.request.headers["If-Modified-Since"]){
-            var lastModified = parseInt(new Date(skinFile.lastModifiedTime).getTime() / 1000);
-            var ifLastModified = parseInt(new Date(
-                    aServerHandler.request.headers["If-Modified-Since"]).getTime() / 1000);
 
-            if(lastModified == ifLastModified){
+        // Send 304 if possible
+        if(aServerHandler.request.headers["If-Modified-Since"]){
+            let ifModifiedSince = new Date(aServerHandler.request.headers["If-Modified-Since"]).toUTCString();
+
+            if(lastModified === ifModifiedSince){
                 aServerHandler.response.writeHeaders(304);
                 aServerHandler.close();
                 return;
             }
         }
 
-        //Content-Typeの設定
-        var contentType = 'text/plain';
+
+        // Set Content-Type
+        var contentType;
+
         try{
             let mimeService = Cc["@mozilla.org/uriloader/external-helper-app-service;1"]
-                    .getService(Ci.nsIMIMEService);
+                                .getService(Ci.nsIMIMEService);
             contentType = mimeService.getTypeFromFile(skinFile);
-        }catch(ex){}
+        }catch(ex){
+            contentType = 'text/plain';
+        }
+
         aServerHandler.response.setHeader("Content-Type", contentType);
 
+
+        // Send 200
         aServerHandler.response.writeHeaders(200);
 
-        //ファイル送信
-        var fileStream = Cc["@mozilla.org/network/file-input-stream;1"]
-                .createInstance(Ci.nsIFileInputStream);
+
+        // Send the skin file
+        var fileStream = Cc["@mozilla.org/network/file-input-stream;1"].createInstance(Ci.nsIFileInputStream);
+
         fileStream.init(skinFile, 0x01, 0444, fileStream.CLOSE_ON_EOF);
         aServerHandler.response.stream.writeFrom(fileStream, skinFile.fileSize);
 
@@ -69,13 +73,17 @@ SkinServerScript.prototype = {
         aServerHandler.close();
     },
 
+
     cancel: function(){
     },
 
-    resolveSkinFile: function(aFilePath){
-        var skinName = ChaikaCore.pref.getUniChar("thread_skin");
 
-        var skinFile = null;
+    resolveSkinFile: function(aFilePath){
+        if(!aFilePath) return null;
+
+        var skinName = ChaikaCore.pref.getUniChar("thread_skin");
+        var skinFile;
+
         if(skinName){
             skinFile = ChaikaCore.getDataDir();
             skinFile.appendRelativePath("skin");
