7623d1df38d2b74b07da676104428b10f343e41c
nodaguti <nodaguti@gmail.com>
2015-05-25 16:39:40 +0900

    Add a blank hander for /thread/
    - A simple message for a thread page.
    - An 400 Bad Request error page for a non-thread page.
    - The favicon image is available under `http://localhost:port/icon.png`

--- chaika/modules/LocalServer.js
+++ chaika/modules/LocalServer.js
@@ -9,9 +9,10 @@ const { interfaces: Ci, classes: Cc, results: Cr, utils: Cu } = Components;
 let { Services } = Cu.import("resource://gre/modules/Services.jsm", {});
 let { FileUtils } = Cu.import('resource://gre/modules/FileUtils.jsm', {});
 let { OS } = Cu.import("resource://gre/modules/osfile.jsm", {});
-let { HttpServer } = Cu.import('resource://chaika-modules/libs/httpd.js', {});
+let { HttpServer, HTTP_400, HTTP_404 } = Cu.import('resource://chaika-modules/libs/httpd.js', {});
 let { Prefs } = Cu.import('resource://chaika-modules/utils/Prefs.js', {});
 let { FileIO } = Cu.import('resource://chaika-modules/utils/FileIO.js', {});
+let { URLUtils } = Cu.import('resource://chaika-modules/utils/URLUtils.js', {});
 let { Logger } = Cu.import("resource://chaika-modules/utils/Logger.js", {});
 
 
@@ -49,7 +50,9 @@ let LocalServer = {
      */
     _start(port) {
         this._listen(port);
+        this._registerThreadHandler();
         this._registerSkinDirectory();
+        this._registerThreadErrorHandler();
     },
 
 
@@ -101,6 +104,15 @@ let LocalServer = {
     },
 
 
+    _registerThreadHandler() {
+        let iconPath = OS.Path.join(FileIO.Path.addonDir, 'chrome', 'content', 'chaika', 'icon.png');
+        let iconFile = new FileUtils.File(iconPath);
+
+        this._server.registerPrefixHandler('/thread/', ThreadHandler);
+        this._server.registerFile('/icon.png', iconFile);
+    },
+
+
     _registerSkinDirectory() {
         let path = this._getSkinDir();
         let skinDir = new FileUtils.File(path);
@@ -126,6 +138,12 @@ let LocalServer = {
     },
 
 
+    _registerThreadErrorHandler() {
+        this._server.registerErrorHandler(400, ThreadErrorHandler[400]);
+        this._server.registerErrorHandler(404, ThreadErrorHandler[404]);
+    },
+
+
     observe(aSubject, aTopic, aData) {
         switch(aData){
             case 'server.port': {
@@ -139,6 +157,87 @@ let LocalServer = {
             }
             break;
         }
+    }
+
+};
+
+
+
+/**
+ * a handler for HTTP requests of a BBS thread.
+ */
+let ThreadHandler = {
+
+    handle(request, response) {
+        let url = request.path.replace(/^\/thread\//, '');
+
+        if(!URLUtils.isThread(url)){
+            // Send 400 Bad Request if the specified page is not a thread.
+            throw HTTP_400;
+        }
+
+        response.processAsync();
+        response.setHeader("Content-Type", "text/html; charset=Shift_JIS");
+
+        //let thread = new Thread(threadURL);
+
+        response.write(`${url} is a thread!`);
+        response.finish();
+    }
+
+};
+
+
+
+let ThreadErrorHandler = {
+
+    /**
+     * 400 Bad Request, in our cases this means
+     * the user specified URL doesn't represent a thread page.
+     */
+    400(metadata, response) {
+        response.setStatusLine('1.1', 400, 'Bad Request');
+        response.setHeader("Content-Type", "text/html;charset=utf-8", false);
+
+        let url = FileIO.escapeHTML(metadata.path.replace(/^\/thread\//, ''));
+        let body = `
+            <html>
+                <head>
+                    <meta charset="utf-8" />
+                    <link rel="shortcut icon" href="/icon.png" />
+                    <title>400 Bad Request [chaika]</title>
+                    <style>
+                        body{
+                            background-color: #eee;
+                            color: black;
+                            font-size: 13.5px;
+                            margin: 1em 3em;
+                        }
+                        ul{
+                            list-style-type: square;
+                        }
+                        p{
+                            padding-left: 1em;
+                        }
+                        .url{
+                            font-family: monospace;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h1>400 Bad Request</h1>
+                    <p>
+                        指定された URL <span class="url">${url}</span>
+                        はスレッドを表していないようです。
+                    </p>
+                    <h2>解決策</h2>
+                    <ul>
+                        <li><a href="${url}">chaika を用いないでページを表示する</a></li>
+                    </ul>
+                </body>
+            </html>`;
+
+          response.bodyOutputStream.write(body, body.length);
     }
 
 };
