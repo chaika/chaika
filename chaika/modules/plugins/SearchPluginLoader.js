cef02cb9ed9a443232c86b36a834dbd9f09650e5
nodaguti <nodaguti@gmail.com>
2015-06-01 17:42:07 +0900

    Remove unnecessary module imports.

--- chaika/modules/plugins/SearchPluginLoader.js
+++ chaika/modules/plugins/SearchPluginLoader.js
@@ -6,9 +6,7 @@ this.EXPORTED_SYMBOLS = ["SearchPluginLoader"];
 
 const { interfaces: Ci, classes: Cc, results: Cr, utils: Cu } = Components;
 
-let { Services } = Cu.import("resource://gre/modules/Services.jsm", {});
 let { AbstractPluginLoader } = Cu.import('resource://chaika-modules/plugins/AbstractPluginLoader.js', {});
-let { Logger } = Cu.import("resource://chaika-modules/utils/Logger.js", {});
 
 
 let SearchPluginLoader = Object.create(AbstractPluginLoader, {
