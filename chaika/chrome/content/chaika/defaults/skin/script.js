6779470e2168fa7643c85a98737f04c207825e5f
nodaguti <nodaguti@gmail.com>
2015-06-13 12:09:57 +0900

    the same as 2766236

--- chaika/chrome/content/chaika/defaults/skin/script.js
+++ chaika/chrome/content/chaika/defaults/skin/script.js
@@ -549,7 +549,7 @@ var ResInfo = {
             // ID別発言数
             if(enablePostsCount){
                 let id = resNode.dataset.id;
-                let isEmptyID = !!Number.parseInt(id.trim());
+                let isEmptyID = !Number.parseInt(id.trim());
                 let isAnonymousID = id.startsWith('???');
 
                 if(id && !isEmptyID && !isAnonymousID){
